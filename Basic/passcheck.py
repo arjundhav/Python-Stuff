c652d7826d67e032ef18dc24849136fef2e853e8
Arjun Jadhav <63765264+arjundhav@users.noreply.github.com>
2021-07-01 11:24:36 +0530

    Delete passcheck.py

--- Basic/passcheck.py
+++
@@ -1,8 +0,0 @@
-user=input('Whats ur User Id: ')
-password=input('Enter ur Password: ')
-
-password_length=len(password)
-hidden_pass='*' * password_length
-
-print(f'{user},your password {hidden_pass},is {password_length} letters long')
-
